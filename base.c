09c456965674dd4489a65004d009d42193f8e401
Isidora Reyes <isidora.reyes-int@zippedi.com>
2026-09-15 16:41:13 -0300

    Se añade generador de grafo

---
+++ base.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+/* Documentacion */
+typedef struct NodoGrafo{
+    int nombre; //ver posibilidad de interconexion entre grafos (malo)
+    float conexiones[100]; 
+}NodoGrafo;
+
+typedef struct Grafo{
+    NodoGrafo nodos[100];
+    int numeroNodos;
+}Grafo;
+
+
+Grafo crearGrafo(int numNodos){
+    struct Grafo grafo;
+    grafo.numeroNodos = 0;
+    while (grafo.numeroNodos < numNodos){
+       agregarNodo(&grafo);
+    }
+    return grafo;
+}
+
+void agregarNodo(Grafo *grafo){
+    struct NodoGrafo nodo;
+    nodo.nombre = grafo->numeroNodos;
+    nodo.conexiones[grafo->numeroNodos]= 0.0;
+
+    for (int i=0; i < grafo->numeroNodos; i++){
+        nodo.conexiones[i] = INFINITY;
+        grafo->nodos[i].conexiones[grafo->numeroNodos] = INFINITY;
+    }
+    grafo->nodos[grafo->numeroNodos] = nodo;
+    grafo->numeroNodos++;
+}
+
+void crearArista(Grafo *grafo, int nodoA, int nodoB, float peso){
+    grafo->nodos[nodoA].conexiones[nodoB] = peso;
+    grafo->nodos[nodoB].conexiones[nodoA] = peso;
+}
+
+float generarPeso(){
+    return (float)(rand() + 1) / (RAND_MAX + 1.0f);
+}
+
+int indiceLista(int *lista, int cantidad, bool eliminacion){
+    int indice = rand()%cantidad;
+    int indice_elegido = lista[indice];
+    if(eliminacion){
+        lista[indice] = lista[cantidad-1];
+    }
+    return indice_elegido;
+
+}
+
+Grafo generadorAleatorio(int i, int j){
+    int v = pow(2,i);
+    int e = pow(2,j);
+    Grafo grafo = crearGrafo(pow(2,i));
+    int desconectados[v-1];
+    int conectados[v];
+    conectados[0] = 0;
+
+    //inicializando las listas
+    for(int k = 1; k < v; k++){
+        desconectados[k-1] = k;
+    }
+
+    for(int k = 0; k < e; k++){
+        if(k < v-1){
+            float peso = generarPeso();
+            int desconectado = indiceLista(desconectados, v-1-k, true); //hay q elegir elemento de desconectados 
+            int conectado = indiceLista(conectados, k+1, false); //hay q elegir elemento de conectados
+            conectados[k+1] = desconectado;
+            crearArista(&grafo, desconectado, conectado, peso);
+            
+        }
+
+        else{
+            //tomamos dos numeros entre 0 y v-1, que no tengan conexion antes 
+            float peso = generarPeso(); 
+            bool valido = false;
+            int valorA;
+            int valorB;
+            while (!valido){
+                valorA = rand()% v;
+                valorB = rand()% v;
+                if(valorA != valorB) {
+                    if(grafo.nodos[valorA].conexiones[valorB] == INFINITY){
+                        valido = true;
+                    }
+                }
+            }
+            crearArista(&grafo, valorA, valorB, peso);
+        }
+    }
+    return grafo;
+}
